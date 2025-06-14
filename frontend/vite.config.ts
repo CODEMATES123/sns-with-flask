53b143649f4f9abde91fdd4d9092613d3a04e86a
tommy10092525 <YOUR_EMAIL>
2025-06-14 19:04:47 +0900

    shadcn uiの導入

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import {thisBaseURL} from "./src/constants"
+import { resolve } from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:thisBaseURL
+  base:thisBaseURL,
+  resolve:{
+    alias: [{ find: '@', replacement: resolve(__dirname, './src') }]
+  }
 })
